92fa644c24e7dbc799f2d39fb3514aba06d446d8
Filip Egge <filip.egge@gmail.com>
2012-10-22 11:05:23 +0200

    added øving8

---
+++ TDT4120-Python/oving8/mumien.py
@@ -0,0 +1,21 @@
+from sys import stdin
+
+def beste_sti(nm, sans):
+    # SKRIV DIN KODE HER
+    results = []
+    for x in range(nm):
+        results[x] = 0
+    print results
+    
+    
+
+n = int(stdin.readline())
+sansynligheter = [float(x) for x in stdin.readline().split()]
+nabomatrise = []
+for linje in stdin:
+    naborad = [0] * n
+    naboer = [int(nabo) for nabo in linje.split()]
+    for nabo in naboer:
+        naborad[nabo] = 1
+    nabomatrise.append(naborad)
+print beste_sti(nabomatrise, sansynligheter)
